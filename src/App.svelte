a3a3cb4417680485cbf739df6744dc19f52ff1aa
mrshappy0 <ajs11300@gmail.com>
2020-05-05 10:29:34 -0600

    First commit for this project

---
+++ src/App.svelte
@@ -0,0 +1,33 @@
+<script>
+  import Product from "./Product.svelte";
+  import Modal from "./Modal.svelte";
+
+  let showModal = false;
+  let closeable = false;
+
+  let products = [{ id: "p1", title: "Art Book", price: 9.99 }];
+  function addToCart(event) {
+    console.log(event);
+  }
+
+  function deleteProduct(bob) {
+    console.log(bob);
+  }
+</script>
+
+{#each products as product}
+  <Product {...product} on:add-to-cart={addToCart} on:delete={deleteProduct} />
+{/each}
+
+<button on:click={() => (showModal = true)}>Show Modal</button>
+
+{#if showModal}
+  <Modal
+    on:cancel={() => (showModal = false)}
+    on:close={() => (showModal = false)}
+    let:didAgree={closeable}>
+    <h1 slot="header">"fuck you bob"</h1>
+    <p>Yea okay bob!</p>
+    <button slot="footer" on:click={() => (showModal = false)} disabled={!closeable}>Confirm</button>
+  </Modal>
+{/if}
---
+++ src/Product.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+  export let title;
+  export let price;
+  export let id;
+  export let bestseller = false;
+
+
+  const dispatch = createEventDispatcher();
+
+  function addToCart(){
+      dispatch('add-to-cart', {id: 'p1'})
+  }
+</script>
+
+<article id={id}>
+  <h1>{title}</h1>
+  <h2>${price}</h2>
+  <p>{bestseller}</p>
+  <button on:click="{addToCart}">Add to Cart</button>
+  <button on:click={()=>{dispatch('delete', 'p1')}}>Delete</button>
+</article>
