6c6f8cc66586e50534b2e84a42bbfe0c8df16a1b
mrshappy0 <ajs11300@gmail.com>
2020-06-18 19:44:12 -0600

    add changes

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,33 @@
 <script>
+  import { tick } from "svelte";
   import Product from "./Product.svelte";
   import Modal from "./Modal.svelte";
 
   let showModal = false;
   let closeable = false;
+  let text = "This is some dummy text!";
 
   let products = [{ id: "p1", title: "Art Book", price: 9.99 }];
+
+  function transformText() {
+    if (event.which !== 9) {
+      return;
+    }
+    event.preventDefault();
+    const selectionStart = event.target.selectionStart;
+    const selectionEnd = event.target.selectionEnd;
+    const value = event.target.value;
+
+    text =
+      value.slice(0, selectionStart) +
+      value.slice(selectionStart, selectionEnd).toUpperCase() +
+      value.slice(selectionEnd);
+    tick().then(() => {
+      event.target.selectionStart = selectionStart;
+      event.target.selectionEnd = selectionEnd;
+    });
+  }
+
   function addToCart(event) {
     console.log(event);
   }
@@ -28,6 +50,13 @@
     let:didAgree={closeable}>
     <h1 slot="header">"fuck you bob"</h1>
     <p>Yea okay bob!</p>
-    <button slot="footer" on:click={() => (showModal = false)} disabled={!closeable}>Confirm</button>
+    <button
+      slot="footer"
+      on:click={() => (showModal = false)}
+      disabled={!closeable}>
+      Confirm
+    </button>
   </Modal>
 {/if}
+
+<textarea rows="5" value={text} on:keydown={transformText} />
